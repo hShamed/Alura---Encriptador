5ce8b6f9165d85775706e18ceb5490dcde1ed960
Hiram Shamed Ambriz Rubio <h.shamed.ar@gmail.com>
2023-02-10 02:11:41 -0600

    Sitio web apto para 'encriptar' y 'desencriptar'

--- js/script.js
+++ js/script.js
@@ -1,34 +1,60 @@
-/*
-function encriptar(palabra){
-    let arr = Array.from(palabra);
+var texto;
+var areaMostrada = document.getElementById("box");
 
-    for(let l = 0; l < arr.lenght(); l++){
-        if(arr[l] == "e"){
-            arr[l] = "enter";
-        } else if(arr[l] == "i"){
-            arr[l] = "imes";
-        } else if(arr[l] == "a"){
-            arr[l] = "ai";
-        } else if(arr[l] == "o"){
-            arr[l] = "ober";
-        } else if(arr[l] == "u"){
-            arr[l] = "ufat";
-        }
-    }
+function encriptar(){
+    texto = document.getElementById("input").value;
 
-    return palabra;
+    texto = texto.replaceAll("e", "enter");
+    texto = texto.replaceAll("i", "ines");
+    texto = texto.replaceAll("a", "ai");
+    texto = texto.replaceAll("o", "ober");
+    texto = texto.replaceAll("u", "ufat");
+
+    incrustar(texto);
 }
-*/
 
-function seleccionar(){
-    var texto = document.getElementById("input").value;
-    var areaMostrada = document.getElementById("box");
+function desencriptar(){
+    texto = document.getElementById("input").value;
 
+    texto = texto.replaceAll("enter", "e");
+    texto = texto.replaceAll("ines", "i");
+    texto = texto.replaceAll("ai", "a");
+    texto = texto.replaceAll("ober", "o");
+    texto = texto.replaceAll("ufat", "u");
+
+    incrustar(texto);
+}
+
+function incrustar(texto){
     if(texto == ""){
         areaMostrada.innerHTML = "<img src='/Primero/img/null.jpg' alt='Ningún mensaje fue encontrado' class='search'>" +
                                     "<p class='pri-text'>Ningún mensaje fue encontrado</p>" +
                                     "<p class='sub-text'>Ingresa el texto que desees encriptar o desencriptar</p>";
     } else{
-        areaMostrada.innerHTML = texto;
+        areaMostrada.innerHTML = texto +
+                                    "<button class='copiar' onclick='copiar()'>Copiar</button>";
     }
+
+    texto = "";
 }
+/*
+function copiar(texto){
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement("oculto");
+
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+
+    // Selecciona el contenido del campo
+    aux.select();
+
+    // Copia el texto seleccionado
+    document.execCommand("copy");
+
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
+}
+*/
